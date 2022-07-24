a69c8a5ecfc1e25c8a1ce550b5ee1cef08ba417e
John Morrice <john.morrice.developer@gmail.com>
2022-07-24 16:22:45 +0100

    add generate uuid helper

--- learnblog/main.go
+++ learnblog/main.go
@@ -2,13 +2,32 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+
+	"github.com/google/uuid"
 )
 
 var addrFlag = flag.String("addr", "0.0.0.0:8080", "server bind address in form IP:PORT")
+var command = flag.String("command", "serve", "command to run: serve,uuid")
 
 func main() {
 	flag.Parse()
+	switch *command {
+	case "serve":
+		runServer()
+	case "uuid":
+		generateUUID()
+	default:
+		log.Fatalf("unsupported command: %s", *command)
+	}
+}
+
+func generateUUID() {
+	fmt.Println(uuid.NewString())
+}
+
+func runServer() {
 	blogStore := &BlogStore{}
 	blogService := BlogService{
 		Store: blogStore,
