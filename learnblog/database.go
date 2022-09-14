3f14711aaf11f21326899dff32b9518a01e6c1eb
John Morrice <john.morrice.developer@gmail.com>
2022-09-14 17:23:34 +0100

    migrate db up and down

---
+++ learnblog/database.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	pgmigrate "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
+)
+
+const dbDriverName = "postgres"
+
+func openDb(databaseURL string) (*sql.DB, error) {
+	err := validateDatabaseParam()
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := sql.Open(dbDriverName, databaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("error opening postgres connection: %w", err)
+	}
+
+	return db, nil
+}
+
+func openMigration(databaseURL, migrationsPath string) (*migrate.Migrate, error) {
+	db, err := openDb(databaseURL)
+	if err != nil {
+		return nil, err
+	}
+
+	driver, err := pgmigrate.WithInstance(db, &pgmigrate.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("error creating migration driver: %w", err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(migrationsPath, dbDriverName, driver)
+	if err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func migrateDbUp(databaseURL, migrationsPath string) error {
+	migration, err := openMigration(databaseURL, migrationsPath)
+	if err != nil {
+		return err
+	}
+	defer migration.Close()
+	err = migration.Up()
+	if err != nil {
+		return fmt.Errorf("error migrating up: %w", err)
+	}
+	return nil
+}
+
+func migrateDbDown(databaseURL, migrationsPath string) error {
+	migration, err := openMigration(databaseURL, migrationsPath)
+	if err != nil {
+		return err
+	}
+	defer migration.Close()
+	err = migration.Down()
+	if err != nil {
+		return fmt.Errorf("error migrating up: %w", err)
+	}
+	return nil
+}
--- learnblog/main.go
+++ learnblog/main.go
@@ -1,26 +1,18 @@
 package main
 
 import (
-	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/url"
 
-	"github.com/golang-migrate/migrate/v4"
-	migratedb "github.com/golang-migrate/migrate/v4/database"
-	pgmigrate "github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/google/uuid"
-	_ "github.com/lib/pq"
 )
-
-const dbDriverName = "postgres"
 
 var addrFlag = flag.String("addr", "0.0.0.0:8080", "server bind address in form IP:PORT")
 var command = flag.String("command", "serve", "command to run: serve,uuid,migrate")
-var databaseURL = flag.String("database", "", "postgres database URL")
-var migrationsPath = flag.String("migrations", "", "path to migration scripts")
+var databaseURLParam = flag.String("database", "", "postgres database URL")
+var migrationsPathParam = flag.String("migrations", "", "path to migration scripts")
 
 func main() {
 	flag.Parse()
@@ -29,54 +21,21 @@ func main() {
 		runServer()
 	case "uuid":
 		generateUUID()
-	case "migrate":
-		migrateDbUp()
+	case "migrate-up":
+		migrateDbUp(*databaseURLParam, *migrationsPathParam)
+	case "migrate-down":
+		migrateDbDown(*databaseURLParam, *migrationsPathParam)
 	default:
 		log.Fatalf("unsupported command: %s", *command)
 	}
 }
 
 func validateDatabaseParam() error {
-	_, err := url.Parse(*databaseURL)
+	_, err := url.Parse(*databaseURLParam)
 	if err != nil {
 		return fmt.Errorf("failed to parse database URL: %w", err)
 	}
 	return nil
-}
-
-func openDb() (migratedb.Driver, error) {
-	err := validateDatabaseParam()
-	if err != nil {
-		return nil, err
-	}
-
-	db, err := sql.Open(dbDriverName, *databaseURL)
-	if err != nil {
-		return nil, fmt.Errorf("error opening postgres connection: %w", err)
-	}
-	driver, err := pgmigrate.WithInstance(db, &pgmigrate.Config{})
-	if err != nil {
-		return nil, fmt.Errorf("error creating migration driver: %w", err)
-	}
-	return driver, nil
-}
-
-func migrateDbUp() {
-	driver, err := openDb()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	m, err := migrate.NewWithDatabaseInstance(*migrationsPath, dbDriverName, driver)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	err = m.Up()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
 }
 
 func generateUUID() {
