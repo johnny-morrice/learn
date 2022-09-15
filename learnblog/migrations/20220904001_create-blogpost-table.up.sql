4ad4e1d7ed65065a49a719b737a2f6e72c53254f
John Morrice <john.morrice.developer@gmail.com>
2022-09-15 15:34:59 +0100

    add mandatory gorm fields

--- learnblog/migrations/20220904001_create-blogpost-table.up.sql
+++ learnblog/migrations/20220904001_create-blogpost-table.up.sql
@@ -2,5 +2,8 @@ CREATE TABLE blog_posts (
     id SERIAL PRIMARY KEY,
     uuid VARCHAR(36) UNIQUE NOT NULL,
     title TEXT NOT NULL,
-    body TEXT NOT NULL
+    body TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
 );
--- learnblog/migrations/20220907002_create-blogtags.up.sql
+++ learnblog/migrations/20220907002_create-blogtags.up.sql
@@ -1,5 +1,8 @@
 CREATE TABLE blog_tags (
     id SERIAL PRIMARY KEY,
     blog_id INT REFERENCES blogposts(id),
-    tag_id INT REFERENCES tags(id)
+    tag_id INT REFERENCES tags(id),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
 );
