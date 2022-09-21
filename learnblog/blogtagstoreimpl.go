250f4d55ce5fd893bc57e85a688cb0a87b3b9421
juanjvazquez <vazquezj@tcd.ie>
2022-09-21 20:52:35 +0100

    desing AddPostTags method

--- learnblog/blogtagstoreimpl.go
+++ learnblog/blogtagstoreimpl.go
@@ -15,6 +15,7 @@ type Tag struct {
 	Name string
 }
 
+// below should be called PostTag
 type BlogTag struct {
 	gorm.Model
 	TagID      int
@@ -25,4 +26,8 @@ type BlogTag struct {
 
 func (store BlogTagStoreImpl) AddPostTags(ctx context.Context, blogPostID uint, tags []string) error {
 	panic("not implemented")
+	// 1. find tags that already exist
+	// 2. create tags that dont exist
+	// 3. find blogtags that already exist
+	// 4. create the blogtags that dont already exist
 }
