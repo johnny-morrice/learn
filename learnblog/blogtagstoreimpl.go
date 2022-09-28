7a94909b0552c6af73929be1f559a0e59c1eba2b
juanjvazquez <vazquezj@tcd.ie>
2022-09-28 20:47:28 +0100

    created UpdatePostTags function

--- learnblog/blogtagstoreimpl.go
+++ learnblog/blogtagstoreimpl.go
@@ -23,12 +23,110 @@ type PostTag struct {
 	BlogPost   BlogPost
 }
 
-func (store BlogTagStoreImpl) UpdatePostTags(ctx context.Context, blogPostID uint, tags []string) error {
-	panic("not implemented")
+func (store BlogTagStoreImpl) FindTagsByNames(ctx context.Context, tags []string) ([]Tag, error) {
+	out := []Tag{}
+	err := store.DB.Find(&out, "name IN ?", tags).Error
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (store BlogTagStoreImpl) CreateTags(ctx context.Context, tagNames []string) ([]Tag, error) {
+	tags := []Tag{}
+
+	for _, t := range tagNames {
+		tags = append(tags, Tag{Name: t})
+	}
+
+	err := store.DB.Create(&tags).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return tags, nil
+}
+
+func DiffTagNames(tags []Tag, tagNames []string) []string {
+	diff := []string{}
+	existing := map[string]struct{}{}
+
+	for _, t := range tags {
+		existing[t.Name] = struct{}{}
+	}
+	for _, name := range tagNames {
+		_, present := existing[name]
+		if !present {
+			diff = append(diff, name)
+		}
+	}
+
+	return diff
+}
+
+func (store BlogTagStoreImpl) UnlinkedTagsForPost(ctx context.Context, blogPostID uint, tags []string) ([]Tag, error) {
+	out := []Tag{}
+
+	const sql = "SELECT * FROM tags where t.name IN ? AND id NOT IN (SELECT tag_id FROM post_tag where post_id = ?)"
+
+	err := store.DB.Raw(sql, tags, blogPostID).Scan(&out).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
+
+func (store BlogTagStoreImpl) CreatePostTags(ctx context.Context, blogPostID uint, tags []Tag) ([]PostTag, error) {
+	postTags := []PostTag{}
+
+	for _, t := range tags {
+		postTags = append(postTags, PostTag{BlogPostID: int(blogPostID), TagID: int(t.ID)})
+	}
+
+	err := store.DB.Create(&postTags).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return postTags, nil
+}
+
+func (store BlogTagStoreImpl) DeletePostTagsNotInNames(ctx context.Context, blogPostID uint, tagNames []string) error {
+	const sql = "DELETE FROM tags WHERE id IN (SELECT pt.tag_id FROM post_tags pt JOIN tags t ON pt.tag_id = t.id WHERE pt.post_id = ? AND t.name NOT IN ?)"
+
+	return store.DB.Exec(sql, blogPostID, tagNames).Error
+}
+
+func (store BlogTagStoreImpl) UpdatePostTags(ctx context.Context, blogPostID uint, tagNames []string) error {
 	// 1. find tags that already exist
 	// 2. create tags that dont exist
 	// 3. find posttags that already exist
 	// 4. create the posttags that dont already exist
 	// 5. delete posttags that are no longer required
 	// Note: I think it is OK to keep tags that are not linked to posttags anymore
+
+	existingTags, err := store.FindTagsByNames(ctx, tagNames)
+	if err != nil {
+		return err
+	}
+
+	newNames := DiffTagNames(existingTags, tagNames)
+
+	_, err = store.CreateTags(ctx, newNames)
+	if err != nil {
+		return err
+	}
+
+	unlinkedTags, err := store.UnlinkedTagsForPost(ctx, blogPostID, tagNames)
+	if err != nil {
+		return err
+	}
+
+	_, err = store.CreatePostTags(ctx, blogPostID, unlinkedTags)
+	if err != nil {
+		return err
+	}
+
+	return store.DeletePostTagsNotInNames(ctx, blogPostID, tagNames)
 }
