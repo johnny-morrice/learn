4cd5c55ce8fbd526392f8c94e687f4d5772cda89
John Morrice <john.morrice.developer@gmail.com>
2022-10-02 17:54:50 +0100

    no separate bytecode from memory

--- vmlang/vm.go
+++ vmlang/vm.go
@@ -3,11 +3,10 @@ package main
 import "io"
 
 type VmPackage struct {
-	Bytecode []Bytecode
-	Memory   []uint64
-	Output   io.Writer
-	SP       uint64
-	IP       uint64
+	Memory []uint64
+	Output io.Writer
+	SP     uint64
+	IP     uint64
 }
 
 func (vm *VmPackage) Execute() error {
